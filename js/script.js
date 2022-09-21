d8e2bbdcd060fa3e5eb2b1c96db483b4bac486c5
Ultimate-force <eyvazovalikamran16@gmail.com>
2022-09-22 01:11:50 +0400

    countdown на best deals section добавлен

--- js/script.js
+++ js/script.js
@@ -249,125 +249,166 @@ $(document).ready(function(){
   ]
       });
 
-
-
-    $('.featured-slider-bestselling').slick({
-        infinite: false,
-        dots: false,
-        slidesToShow: 5,  
-        slidesToScroll: 1,
-        adaptiveHeight: true,
-        draggable: true,
-        speed: 200,
-        appendArrows:$('.slider-header-filter-2'),
-           responsive: [
-           {
-              breakpoint: 1250,
-              settings: {
-                  slidesToShow: 4
-             }
-           },{
-              breakpoint: 1020,
-              settings: {
-                 slidesToShow: 3
-             }
-           },{
-              breakpoint: 768,
-              settings: {
-                 slidesToShow: 2
-             }
-          },{
-              breakpoint: 540,
-              settings: {
-                 slidesToShow: 1
-             }
-          }
-  ]
-      });
-
-    $('.featured-slider-latest').slick({
-        infinite: false,
-        dots: false,
-        slidesToShow: 5,  
-        slidesToScroll: 1,
-        adaptiveHeight: true,
-        draggable: true,
-        speed: 200,
-        appendArrows:$('.slider-header-filter-3'),
-           responsive: [
-           {
-              breakpoint: 1250,
-              settings: {
-                  slidesToShow: 4
-             }
-           },{
-              breakpoint: 1020,
-              settings: {
-                 slidesToShow: 3
-             }
-           },{
-              breakpoint: 768,
-              settings: {
-                 slidesToShow: 2
-             }
-          },{
-              breakpoint: 540,
-              settings: {
-                 slidesToShow: 1
-             }
-          }
-  ]
-      });
-
-
-    // Featured slider filters
+    // Featured slider filters --active
 
     $(document).on('click', '.featured-filters span', function(){
         $(this).addClass('active').siblings().removeClass('active')
     });
 
-
-
-    $('.featured-slider-bestselling').fadeOut();
-    $('.featured-slider-latest').fadeOut();  
-
-    $('.slider-header-filter-2').fadeOut();  
-    $('.slider-header-filter-3').fadeOut();  
-    
-    $('.featured').click(function(){
-        $('.slider-header-filter').fadeIn(1000);  
-        $('.slider-header-filter-2').fadeOut(5);  
-        $('.slider-header-filter-3').fadeOut(5);  
-        $('.featured-slider-bestselling').fadeOut(5); // Скрывает начальное содержимое.
-        $('.featured-slider-latest').fadeOut(5); // Показывает содержимое диалога.
-        $('.featured-slider-featured').fadeIn(1000); // Показывает содержимое диалога.
-    });
-
-    $('.bestselling').click(function(){
-        $('.slider-header-filter').fadeOut(5);  
-        $('.slider-header-filter-2').fadeIn(1000);
-        $('.slider-header-filter-3').fadeOut(5);    
-        $('.featured-slider-featured').fadeOut(5); // Скрывает начальное содержимое.
-        $('.featured-slider-bestselling').fadeIn(1000); // Скрывает начальное содержимое.
-        $('.featured-slider-latest').fadeOut(5); // Показывает содержимое диалога.
-    });
-
-    $('.latest').click(function(){
-        $('.slider-header-filter').fadeOut(5);  
-        $('.slider-header-filter-2').fadeOut(5);  
-        $('.slider-header-filter-3').fadeIn(1000);  
-        $('.featured-slider-bestselling').fadeOut(5);  
-        $('.featured-slider-latest').fadeIn(1000); 
-        $('.featured-slider-featured').fadeOut(5);   
-    });
-
-     
-
+  
 
 });
 
 
+let countDownDate = new Date("October 14, 2023 15:37:25").getTime();
+
+
+let x = setInterval(function() {
+
+  
+  let now = new Date().getTime();
     
+  
+  let distance = countDownDate - now;
+    
+  
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+   
+  document.getElementById("days").innerHTML = days;
+  
+  document.getElementById("hours").innerHTML = hours;
+
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+    
+  
+   
+}, 1000);
+
+
+let countDownDateTwo = new Date("October 23, 2023 15:37:25").getTime();
+
+
+let a = setInterval(function() {
+
+  
+  let nowTwo = new Date().getTime();
+    
+  
+  let distanceTwo = countDownDateTwo - nowTwo;
+    
+  
+  let daysTwo = Math.floor(distanceTwo / (1000 * 60 * 60 * 24));
+  let hoursTwo = Math.floor((distanceTwo % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutesTwo = Math.floor((distanceTwo % (1000 * 60 * 60)) / (1000 * 60));
+  let secondsTwo = Math.floor((distanceTwo % (1000 * 60)) / 1000);
+    
+   
+  document.getElementById("daysTwo").innerHTML = daysTwo;
+  
+  document.getElementById("hoursTwo").innerHTML = hoursTwo;
+
+  document.getElementById("minutesTwo").innerHTML = minutesTwo;
+  document.getElementById("secondsTwo").innerHTML = secondsTwo;
+    
+  
+   
+}, 1000);
+
+
+let countDownDateThree = new Date("October 30, 2023 15:37:25").getTime();
+
+
+let b = setInterval(function() {
+
+  
+  let nowThree = new Date().getTime();
+    
+  
+  let distanceThree = countDownDateThree - nowThree;
+    
+  
+  let daysThree = Math.floor(distanceThree / (1000 * 60 * 60 * 24));
+  let hoursThree = Math.floor((distanceThree % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutesThree = Math.floor((distanceThree % (1000 * 60 * 60)) / (1000 * 60));
+  let secondsThree = Math.floor((distanceThree % (1000 * 60)) / 1000);
+    
+   
+  document.getElementById("daysThree").innerHTML = daysThree;
+  
+  document.getElementById("hoursThree").innerHTML = hoursThree;
+
+  document.getElementById("minutesThree").innerHTML = minutesThree;
+  document.getElementById("secondsThree").innerHTML = secondsThree;
+    
+  
+   
+}, 1000);
+
+
+let countDownDateFour = new Date("Dec 25, 2022 15:37:25").getTime();
+
+
+let c = setInterval(function() {
+
+  
+  let nowFour = new Date().getTime();
+    
+  
+  let distanceFour = countDownDateFour - nowFour;
+    
+  
+  let daysFour = Math.floor(distanceFour / (1000 * 60 * 60 * 24));
+  let hoursFour = Math.floor((distanceFour % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutesFour = Math.floor((distanceFour % (1000 * 60 * 60)) / (1000 * 60));
+  let secondsFour = Math.floor((distanceFour % (1000 * 60)) / 1000);
+    
+   
+  document.getElementById("daysFour").innerHTML = daysFour;
+  
+  document.getElementById("hoursFour").innerHTML = hoursFour;
+
+  document.getElementById("minutesFour").innerHTML = minutesFour;
+  document.getElementById("secondsFour").innerHTML = secondsFour;
+    
+  
+   
+}, 1000);
+
+
+let countDownDateFive = new Date("October 14, 2024 15:37:25").getTime();
+
+
+let d = setInterval(function() {
+
+  
+  let nowFive = new Date().getTime();
+    
+  
+  let distanceFive = countDownDateFive - nowFive;
+    
+  
+  let daysFive = Math.floor(distanceFive / (1000 * 60 * 60 * 24));
+  let hoursFive = Math.floor((distanceFive % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutesFive = Math.floor((distanceFive % (1000 * 60 * 60)) / (1000 * 60));
+  let secondsFive = Math.floor((distanceFive % (1000 * 60)) / 1000);
+    
+   
+  document.getElementById("daysFive").innerHTML = daysFive;
+  
+  document.getElementById("hoursFive").innerHTML = hoursFive;
+
+  document.getElementById("minutesFive").innerHTML = minutesFive;
+  document.getElementById("secondsFive").innerHTML = secondsFive;
+    
+  
+   
+}, 1000);
+
 
  
 
